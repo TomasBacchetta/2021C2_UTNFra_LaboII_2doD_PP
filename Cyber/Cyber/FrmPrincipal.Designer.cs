817c609df2babb82bc3d378350eb81d723792a08
TomasBacchetta <78171375+TomasBacchetta@users.noreply.github.com>
2021-09-26 20:26:52 -0300

    26/9
    26/9

--- Cyber/Cyber/FrmPrincipal.Designer.cs
+++ Cyber/Cyber/FrmPrincipal.Designer.cs
@@ -29,7 +29,7 @@ namespace Cyber
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBoxCompuradoras = new System.Windows.Forms.GroupBox();
+            this.groupBoxEquipos = new System.Windows.Forms.GroupBox();
             this.radioButton15 = new System.Windows.Forms.RadioButton();
             this.radioButton10 = new System.Windows.Forms.RadioButton();
             this.radioButton14 = new System.Windows.Forms.RadioButton();
@@ -45,38 +45,44 @@ namespace Cyber
             this.radioButton3 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.lblCliente = new System.Windows.Forms.Label();
+            this.labelComputadoras = new System.Windows.Forms.Label();
+            this.labelCabinas = new System.Windows.Forms.Label();
             this.buttonMostrarEquipo = new System.Windows.Forms.Button();
-            this.groupBoxCompuradoras.SuspendLayout();
+            this.richTextBoxDatosCliente = new System.Windows.Forms.RichTextBox();
+            this.buttonEcharCliente = new System.Windows.Forms.Button();
+            this.buttonAsignar = new System.Windows.Forms.Button();
+            this.groupBoxEquipos.SuspendLayout();
             this.SuspendLayout();
             // 
-            // groupBoxCompuradoras
+            // groupBoxEquipos
             // 
-            this.groupBoxCompuradoras.Controls.Add(this.radioButton15);
-            this.groupBoxCompuradoras.Controls.Add(this.radioButton10);
-            this.groupBoxCompuradoras.Controls.Add(this.radioButton14);
-            this.groupBoxCompuradoras.Controls.Add(this.radioButton13);
-            this.groupBoxCompuradoras.Controls.Add(this.radioButton9);
-            this.groupBoxCompuradoras.Controls.Add(this.radioButton12);
-            this.groupBoxCompuradoras.Controls.Add(this.radioButton8);
-            this.groupBoxCompuradoras.Controls.Add(this.radioButton11);
-            this.groupBoxCompuradoras.Controls.Add(this.radioButton7);
-            this.groupBoxCompuradoras.Controls.Add(this.radioButton6);
-            this.groupBoxCompuradoras.Controls.Add(this.radioButton5);
-            this.groupBoxCompuradoras.Controls.Add(this.radioButton4);
-            this.groupBoxCompuradoras.Controls.Add(this.radioButton3);
-            this.groupBoxCompuradoras.Controls.Add(this.radioButton2);
-            this.groupBoxCompuradoras.Controls.Add(this.radioButton1);
-            this.groupBoxCompuradoras.Location = new System.Drawing.Point(22, 22);
-            this.groupBoxCompuradoras.Name = "groupBoxCompuradoras";
-            this.groupBoxCompuradoras.Size = new System.Drawing.Size(610, 272);
-            this.groupBoxCompuradoras.TabIndex = 0;
-            this.groupBoxCompuradoras.TabStop = false;
-            this.groupBoxCompuradoras.Text = "Equipos";
+            this.groupBoxEquipos.Controls.Add(this.radioButton15);
+            this.groupBoxEquipos.Controls.Add(this.radioButton10);
+            this.groupBoxEquipos.Controls.Add(this.radioButton14);
+            this.groupBoxEquipos.Controls.Add(this.radioButton13);
+            this.groupBoxEquipos.Controls.Add(this.radioButton9);
+            this.groupBoxEquipos.Controls.Add(this.radioButton12);
+            this.groupBoxEquipos.Controls.Add(this.radioButton8);
+            this.groupBoxEquipos.Controls.Add(this.radioButton11);
+            this.groupBoxEquipos.Controls.Add(this.radioButton7);
+            this.groupBoxEquipos.Controls.Add(this.radioButton6);
+            this.groupBoxEquipos.Controls.Add(this.radioButton5);
+            this.groupBoxEquipos.Controls.Add(this.radioButton4);
+            this.groupBoxEquipos.Controls.Add(this.radioButton3);
+            this.groupBoxEquipos.Controls.Add(this.radioButton2);
+            this.groupBoxEquipos.Controls.Add(this.radioButton1);
+            this.groupBoxEquipos.Location = new System.Drawing.Point(152, 37);
+            this.groupBoxEquipos.Name = "groupBoxEquipos";
+            this.groupBoxEquipos.Size = new System.Drawing.Size(620, 335);
+            this.groupBoxEquipos.TabIndex = 0;
+            this.groupBoxEquipos.TabStop = false;
+            this.groupBoxEquipos.Text = "Equipos";
             // 
             // radioButton15
             // 
             this.radioButton15.AutoSize = true;
-            this.radioButton15.Location = new System.Drawing.Point(487, 221);
+            this.radioButton15.Location = new System.Drawing.Point(487, 273);
             this.radioButton15.Name = "radioButton15";
             this.radioButton15.Size = new System.Drawing.Size(100, 19);
             this.radioButton15.TabIndex = 6;
@@ -87,7 +93,7 @@ namespace Cyber
             // radioButton10
             // 
             this.radioButton10.AutoSize = true;
-            this.radioButton10.Location = new System.Drawing.Point(487, 163);
+            this.radioButton10.Location = new System.Drawing.Point(487, 193);
             this.radioButton10.Name = "radioButton10";
             this.radioButton10.Size = new System.Drawing.Size(100, 19);
             this.radioButton10.TabIndex = 9;
@@ -98,7 +104,7 @@ namespace Cyber
             // radioButton14
             // 
             this.radioButton14.AutoSize = true;
-            this.radioButton14.Location = new System.Drawing.Point(372, 221);
+            this.radioButton14.Location = new System.Drawing.Point(372, 273);
             this.radioButton14.Name = "radioButton14";
             this.radioButton14.Size = new System.Drawing.Size(100, 19);
             this.radioButton14.TabIndex = 5;
@@ -109,7 +115,7 @@ namespace Cyber
             // radioButton13
             // 
             this.radioButton13.AutoSize = true;
-            this.radioButton13.Location = new System.Drawing.Point(254, 221);
+            this.radioButton13.Location = new System.Drawing.Point(254, 273);
             this.radioButton13.Name = "radioButton13";
             this.radioButton13.Size = new System.Drawing.Size(100, 19);
             this.radioButton13.TabIndex = 4;
@@ -120,7 +126,7 @@ namespace Cyber
             // radioButton9
             // 
             this.radioButton9.AutoSize = true;
-            this.radioButton9.Location = new System.Drawing.Point(372, 163);
+            this.radioButton9.Location = new System.Drawing.Point(372, 193);
             this.radioButton9.Name = "radioButton9";
             this.radioButton9.Size = new System.Drawing.Size(94, 19);
             this.radioButton9.TabIndex = 8;
@@ -131,7 +137,7 @@ namespace Cyber
             // radioButton12
             // 
             this.radioButton12.AutoSize = true;
-            this.radioButton12.Location = new System.Drawing.Point(143, 221);
+            this.radioButton12.Location = new System.Drawing.Point(143, 273);
             this.radioButton12.Name = "radioButton12";
             this.radioButton12.Size = new System.Drawing.Size(100, 19);
             this.radioButton12.TabIndex = 3;
@@ -142,7 +148,7 @@ namespace Cyber
             // radioButton8
             // 
             this.radioButton8.AutoSize = true;
-            this.radioButton8.Location = new System.Drawing.Point(254, 163);
+            this.radioButton8.Location = new System.Drawing.Point(254, 193);
             this.radioButton8.Name = "radioButton8";
             this.radioButton8.Size = new System.Drawing.Size(94, 19);
             this.radioButton8.TabIndex = 7;
@@ -153,7 +159,7 @@ namespace Cyber
             // radioButton11
             // 
             this.radioButton11.AutoSize = true;
-            this.radioButton11.Location = new System.Drawing.Point(30, 221);
+            this.radioButton11.Location = new System.Drawing.Point(30, 273);
             this.radioButton11.Name = "radioButton11";
             this.radioButton11.Size = new System.Drawing.Size(100, 19);
             this.radioButton11.TabIndex = 1;
@@ -164,7 +170,7 @@ namespace Cyber
             // radioButton7
             // 
             this.radioButton7.AutoSize = true;
-            this.radioButton7.Location = new System.Drawing.Point(143, 163);
+            this.radioButton7.Location = new System.Drawing.Point(143, 193);
             this.radioButton7.Name = "radioButton7";
             this.radioButton7.Size = new System.Drawing.Size(94, 19);
             this.radioButton7.TabIndex = 6;
@@ -175,7 +181,7 @@ namespace Cyber
             // radioButton6
             // 
             this.radioButton6.AutoSize = true;
-            this.radioButton6.Location = new System.Drawing.Point(30, 163);
+            this.radioButton6.Location = new System.Drawing.Point(30, 193);
             this.radioButton6.Name = "radioButton6";
             this.radioButton6.Size = new System.Drawing.Size(94, 19);
             this.radioButton6.TabIndex = 5;
@@ -238,9 +244,38 @@ namespace Cyber
             this.radioButton1.Text = "radioButton1";
             this.radioButton1.UseVisualStyleBackColor = true;
             // 
+            // lblCliente
+            // 
+            this.lblCliente.AutoSize = true;
+            this.lblCliente.Location = new System.Drawing.Point(789, 41);
+            this.lblCliente.Name = "lblCliente";
+            this.lblCliente.Size = new System.Drawing.Size(96, 15);
+            this.lblCliente.TabIndex = 12;
+            this.lblCliente.Text = "Cliente a atender";
+            // 
+            // labelComputadoras
+            // 
+            this.labelComputadoras.AutoSize = true;
+            this.labelComputadoras.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelComputadoras.Location = new System.Drawing.Point(24, 230);
+            this.labelComputadoras.Name = "labelComputadoras";
+            this.labelComputadoras.Size = new System.Drawing.Size(122, 21);
+            this.labelComputadoras.TabIndex = 11;
+            this.labelComputadoras.Text = "Computadoras";
+            // 
+            // labelCabinas
+            // 
+            this.labelCabinas.AutoSize = true;
+            this.labelCabinas.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelCabinas.Location = new System.Drawing.Point(46, 88);
+            this.labelCabinas.Name = "labelCabinas";
+            this.labelCabinas.Size = new System.Drawing.Size(70, 21);
+            this.labelCabinas.TabIndex = 10;
+            this.labelCabinas.Text = "Cabinas";
+            // 
             // buttonMostrarEquipo
             // 
-            this.buttonMostrarEquipo.Location = new System.Drawing.Point(700, 106);
+            this.buttonMostrarEquipo.Location = new System.Drawing.Point(409, 378);
             this.buttonMostrarEquipo.Name = "buttonMostrarEquipo";
             this.buttonMostrarEquipo.Size = new System.Drawing.Size(97, 46);
             this.buttonMostrarEquipo.TabIndex = 2;
@@ -248,25 +283,62 @@ namespace Cyber
             this.buttonMostrarEquipo.UseVisualStyleBackColor = true;
             this.buttonMostrarEquipo.Click += new System.EventHandler(this.buttonMostrarEquipo_Click);
             // 
+            // richTextBoxDatosCliente
+            // 
+            this.richTextBoxDatosCliente.Location = new System.Drawing.Point(789, 59);
+            this.richTextBoxDatosCliente.Name = "richTextBoxDatosCliente";
+            this.richTextBoxDatosCliente.Size = new System.Drawing.Size(169, 227);
+            this.richTextBoxDatosCliente.TabIndex = 3;
+            this.richTextBoxDatosCliente.Text = "";
+            // 
+            // buttonEcharCliente
+            // 
+            this.buttonEcharCliente.Location = new System.Drawing.Point(894, 292);
+            this.buttonEcharCliente.Name = "buttonEcharCliente";
+            this.buttonEcharCliente.Size = new System.Drawing.Size(64, 23);
+            this.buttonEcharCliente.TabIndex = 5;
+            this.buttonEcharCliente.Text = "Echar";
+            this.buttonEcharCliente.UseVisualStyleBackColor = true;
+            this.buttonEcharCliente.Click += new System.EventHandler(this.buttonEcharCliente_Click);
+            // 
+            // buttonAsignar
+            // 
+            this.buttonAsignar.Location = new System.Drawing.Point(789, 292);
+            this.buttonAsignar.Name = "buttonAsignar";
+            this.buttonAsignar.Size = new System.Drawing.Size(64, 23);
+            this.buttonAsignar.TabIndex = 6;
+            this.buttonAsignar.Text = "Asignar";
+            this.buttonAsignar.UseVisualStyleBackColor = true;
+            this.buttonAsignar.Click += new System.EventHandler(this.buttonAsignar_Click);
+            // 
             // FrmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(825, 385);
+            this.BackgroundImage = global::Cyber.Properties.Resources.big_poster_filme_matrix_reloaded_lo01_tamanho_90x60_cm_matrix_2;
+            this.ClientSize = new System.Drawing.Size(980, 450);
+            this.Controls.Add(this.lblCliente);
+            this.Controls.Add(this.labelComputadoras);
+            this.Controls.Add(this.buttonAsignar);
+            this.Controls.Add(this.labelCabinas);
+            this.Controls.Add(this.buttonEcharCliente);
+            this.Controls.Add(this.richTextBoxDatosCliente);
             this.Controls.Add(this.buttonMostrarEquipo);
-            this.Controls.Add(this.groupBoxCompuradoras);
+            this.Controls.Add(this.groupBoxEquipos);
             this.Name = "FrmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmPrincipal";
             this.Load += new System.EventHandler(this.FrmPrincipal_Load);
-            this.groupBoxCompuradoras.ResumeLayout(false);
-            this.groupBoxCompuradoras.PerformLayout();
+            this.groupBoxEquipos.ResumeLayout(false);
+            this.groupBoxEquipos.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBoxCompuradoras;
+        private System.Windows.Forms.GroupBox groupBoxEquipos;
         private System.Windows.Forms.RadioButton radioButton10;
         private System.Windows.Forms.RadioButton radioButton9;
         private System.Windows.Forms.RadioButton radioButton8;
@@ -283,5 +355,11 @@ namespace Cyber
         private System.Windows.Forms.RadioButton radioButton12;
         private System.Windows.Forms.RadioButton radioButton11;
         private System.Windows.Forms.Button buttonMostrarEquipo;
+        private System.Windows.Forms.RichTextBox richTextBoxDatosCliente;
+        private System.Windows.Forms.Button buttonEcharCliente;
+        private System.Windows.Forms.Button buttonAsignar;
+        private System.Windows.Forms.Label labelComputadoras;
+        private System.Windows.Forms.Label labelCabinas;
+        private System.Windows.Forms.Label lblCliente;
     }
 }
---
+++ Cyber/Sesion/Sesion.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Personas;
+using Equipos;
+using System.Text;
+
+namespace Sesiones
+{
+    public abstract class Sesion
+    {
+        protected Cliente usuarioActual;
+        protected string idEquipo;
+        protected DateTime tiempoInicio;
+        protected DateTime tiempoFinUso;
+        protected double costoTotal;
+        protected bool enCurso;
+
+
+        public string IdEquipo
+        {
+            get
+            {
+                return this.idEquipo;
+            }
+        }
+        public Cliente UsuarioActual
+        {
+            get
+            {
+                return this.usuarioActual;
+            }
+        }
+        public double CostoTotal
+        {
+            get
+            {
+                return this.costoTotal;
+            }
+            set
+            {
+                
+            }
+        }
+        public DateTime TiempoInicio
+        {
+            get
+            {
+                return this.tiempoInicio;
+            }
+            set
+            {
+                this.tiempoInicio = value;
+            }
+        }
+        public DateTime TiempoFin
+        {
+            get
+            {
+                return this.TiempoFin;
+            }
+            set
+            {
+                this.TiempoFin = value;
+            }
+        }
+        
+        public abstract bool EnCurso { get; set; }
+       
+        
+        public Sesion(Cliente usuarioActual, string idEquipo)
+        {
+            this.usuarioActual = usuarioActual;
+            this.idEquipo = idEquipo;
+            this.tiempoInicio = DateTime.Now;
+            this.tiempoFinUso = DateTime.MaxValue;
+            this.enCurso = true;
+            costoTotal = 0;
+        }
+
+        public virtual string MostrarSesion()
+        {
+            StringBuilder buffer = new StringBuilder();
+
+            buffer.AppendLine($"Id Equipo: {this.idEquipo}");
+            buffer.AppendLine($"Usuario: {this.usuarioActual.Nombre} {this.usuarioActual.Apellido}");
+            buffer.AppendLine($"Duración de la sesión: {this.CalcularMinutosPasados()} minutos");
+            buffer.AppendLine($"Monto facturado: ${this.costoTotal}");
+            
+
+            return $"{buffer}";
+        }
+
+
+        public double CalcularMinutosPasados()
+        {
+            long tiempoMinReal;
+            long tiempoSegReal;
+            long tiempoMinRetorno = 0;
+
+            tiempoMinReal = this.tiempoFinUso.Minute - this.tiempoInicio.Minute;
+            tiempoSegReal = this.tiempoFinUso.Second - this.tiempoInicio.Second;
+
+            if (tiempoMinReal < 0)
+            {
+                tiempoMinReal += 60;
+            }
+            if (tiempoSegReal < 0)
+            {
+                tiempoSegReal += 60;
+                tiempoMinReal -= 1;
+            }
+            tiempoMinRetorno = tiempoSegReal + tiempoMinReal * 60;
+
+            return tiempoMinRetorno;
+        }
+
+        
+        
+        
+
+    }
+    
+   
+
+    
+}
