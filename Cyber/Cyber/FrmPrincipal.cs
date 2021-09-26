9e3a66b5b204aa26adf1c2f770880acc6006815b
TomasBacchetta <78171375+TomasBacchetta@users.noreply.github.com>
2021-09-26 20:24:56 -0300

    26/9
    26/9

--- Cyber/Cyber/FrmPrincipal.cs
+++ Cyber/Cyber/FrmPrincipal.cs
@@ -9,12 +9,16 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Equipos;
 using Negocio;
+using Personas;
+using Sesiones;
+using System.Media;
+using Cyber.Properties;
 
 namespace Cyber
 {
     public partial class FrmPrincipal : Form
     {
-        CyberCafe cyber1 = new CyberCafe(10, 5);
+        CyberCafe cyber1 = new CyberCafe(10, 5, 20);
         public FrmPrincipal()
         {
             InitializeComponent();
@@ -22,46 +26,130 @@ namespace Cyber
 
         private void FrmPrincipal_Load(object sender, EventArgs e)
         {
-            foreach (RadioButton item in groupBoxCompuradoras.Controls)
+            foreach (Control item in groupBoxEquipos.Controls)
             {
-                item.Text = cyber1.Equipos[groupBoxCompuradoras.Controls.IndexOf(item)].Id;
+
+                
+                item.Tag = cyber1.Equipos[groupBoxEquipos.Controls.IndexOf(item)].Id;
+                this.ImprimirEtiquetaEquipo(item.Tag.ToString());
+
             }
-
-           
-
+            
+            richTextBoxDatosCliente.Text = cyber1.ObtenerProximoCliente().MostrarCliente();
         }
 
         private void buttonMostrarEquipo_Click(object sender, EventArgs e)
         {
             
-            foreach (RadioButton item in groupBoxCompuradoras.Controls)
+            foreach (RadioButton item in groupBoxEquipos.Controls)
             {
                 if (item.Checked)
                 {
-                    foreach (Equipo equipo in cyber1.Equipos)
+                    FrmVerEquipo form2 = new FrmVerEquipo(cyber1, item.Tag.ToString(), this);
+                    form2.Show();
+                }
+            }
+            
+        }
+
+        private void buttonEcharCliente_Click(object sender, EventArgs e)
+        {
+            
+            if (cyber1.ColaClientes.Count > 0)
+            {
+                cyber1.ColaClientes.Dequeue();
+                this.reproducirSonidoEcharCliente();
+                if (cyber1.ColaClientes.Count == 0)
+                {
+                    richTextBoxDatosCliente.Text = "No hay más clientes";
+                } else
+                {
+                    richTextBoxDatosCliente.Text = cyber1.ObtenerProximoCliente().MostrarCliente();
+                }
+            }
+
+        }
+
+        private void buttonAsignar_Click(object sender, EventArgs e)
+        {
+           
+            int respuesta = 0;
+            string idEquipo = ""; ;
+            if (cyber1.ColaClientes.Count > 0)
+            {
+                foreach (RadioButton item in groupBoxEquipos.Controls)
+                {
+                    if (item.Checked == true)
                     {
-                        if (equipo == item.Text)
-                        {
-                            if (equipo.TipoDeEquipo == Equipo.TipoEquipo.Computadora)
-                            {
-                                Computadora comp = (Computadora)equipo;
-                                FrmVerEquipo form2 = new FrmVerEquipo(comp.Mostrar());
-                                form2.Show();
-                            } else
-                            {
-                                Cabina cabina = (Cabina)equipo;
-                                FrmVerEquipo form2 = new FrmVerEquipo(cabina.Mostrar());
-                                form2.Show();
-                            }
-                            
-                            
-                            
-                        }
+                        idEquipo = item.Tag.ToString();
+                        respuesta = cyber1.AsignarClienteAEquipo(item.Tag.ToString());
+
+
+                        break;
+                    }
+                }
+
+                if (respuesta == 1 || respuesta == 2)
+                {
+                    richTextBoxDatosCliente.Text = cyber1.ObtenerProximoCliente().MostrarCliente();
+                    cyber1.BuscarEquipoPorId(idEquipo).enUso = true;
+                    this.ImprimirEtiquetaEquipo(idEquipo);
+                    this.reproducirSonidoAsignarCliente();
+                    MessageBox.Show("Cliente cargado a la cola del equipo.\nSi este está ocupado deberá esperar");
+
+                }
+                else
+                {
+                    if (respuesta == -1)
+                    {
+                        MessageBox.Show("El cliente no está interesado en esta computadora. \nRecuerde que debe haber presentes por lo menos un favorito de cada categoría");
+                    }
+                    else
+                    {
+                        MessageBox.Show("El cliente no vino para utilizar eso");
                     }
                 }
             }
             
+            
+        }
+
+        public void ImprimirEtiquetaEquipo(string idEquipo)
+        {
+            string estado;
+
+            if (cyber1.BuscarEquipoPorId(idEquipo).enUso == true)
+            {
+                estado = "OCUPADO";
+            } else
+            {
+                estado = "LIBRE";
+            }
+
+            foreach (RadioButton item in groupBoxEquipos.Controls)
+            {
+                if (!(item.Tag is null) && item.Tag.ToString() == idEquipo)
+                {
+                    item.Text = $"{item.Tag} -{estado}\n{cyber1.ObtenerSubColaClientes(idEquipo).Count} clientes en cola";
+                }
+            }
 
         }
+
+
+        private void reproducirSonidoAsignarCliente()
+        {
+            SoundPlayer sonidoAsignarCliente = new SoundPlayer(Cyber.Properties.Resources.YEAH1);
+            sonidoAsignarCliente.Play();
+        }
+        private void reproducirSonidoEcharCliente()
+        {
+            SoundPlayer sonidoEcharCliente = new SoundPlayer(Cyber.Properties.Resources.BOO);
+            sonidoEcharCliente.Play();
+            
+        }
+
+        
+
     }
 }
---
+++ Cyber/Datos/NumeroDeTelefono.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class NumeroTelefono
+    {
+        /*
+         Local: $1.00 por minuto. Se reconoce a una llamada como local cuando el prefijo contiene un '11' o '011' luego del código de país (54 - 11 - #### - ####).
+            Larga distancia: $2.50 por minuto. Se reconoce a una llamada como de larga distancia cuando el prefijo contiene un número distinto de '11' o '011' luego del código de país (54 - #### - #### - ####).
+            Internacional: $5.00 por minuto. Se reconoce a una llamada como de larga distancia cuando el código de país es distinto de '54'.
+         */
+        public string GenerarNumeroTelefono()
+        {
+            Random rnd = new Random();
+            int primerPrefijo;
+            int segundoPrefijo;
+
+            if (rnd.Next(0, 5) > 3)// 1 en 5 chance de que sea internacional
+            {
+                primerPrefijo = rnd.Next(1, 99);
+                segundoPrefijo = rnd.Next(1, 99);
+            }
+            else
+            {
+                primerPrefijo = 54;
+                if (rnd.Next(0, 3) > 0)// 2 en 3 chance de que sea local
+                {
+
+                    segundoPrefijo = 11;
+                }
+                else
+                {
+                    segundoPrefijo = rnd.Next(9, 30);
+                }
+
+            }
+
+            return $"{primerPrefijo}-{segundoPrefijo}-{rnd.Next(2000, 9999)}-{rnd.Next(2000, 9000)}";
+
+        }
+    }
+}
